aa811710109ed53527b41f7fb2b6b7ad86f947cd
Pauline Drouhin <pauline.drouhin@gmail.com>
2019-10-30 18:42:59 +0100

    Click button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import ClickButton from './components/ClickButton';
 
 function App() {
   return (
     <div className="App">
         Happy Halloween
+
+
+        <ClickButton />
     </div>
   );
 }
