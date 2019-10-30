c2de8ac42c42257a329f78d048d1409ffc87f2d0
Ibrahima Niass <cheroubawan@hotmail.fr>
2019-10-31 00:19:54 +0100

    Get random monster

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import './Card.css';
 function Card(props) {
   return (
     <div className="diplay-inline-block" >
-      <img class="img-size single-img" alt={props.name} src={props.src}></img>
+      <img onClick={props.pick} className="img-size single-img" alt={props.name} src={props.src}></img>
     </div>
   );
 }
