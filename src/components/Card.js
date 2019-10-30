d8419040e0f699ebe880bd957d411ae9020c1277
Ibrahima Niass <cheroubawan@hotmail.fr>
2019-10-30 19:29:36 +0100

    made images smaller

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,10 @@
 import React from 'react';
+import './Card.css';
 
 function Card(props) {
   return (
-    <div className="ui three column grid">
-      {/* <div className="column">
-        <div className="ui segment">
-          
-          <div className="content">
-            <a className="header"> {props.name}</a>
-          </div>
-        </div>
-      </div> */}
-      <div className="image">
-        <img class="ui medium circular image" src={props.src} />
-        {/* <img src={props.src} /> */}
-      </div>
+    <div className="">
+      <img class="img-size" src={props.src}></img>
     </div>
   );
 }
