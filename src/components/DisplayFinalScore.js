69303aca629bcfe1981160595253e0c22a45f09b
Pauline Drouhin <pauline.drouhin@gmail.com>
2019-10-31 08:29:56 +0100

    adjusting margins and sizes on last page

--- src/components/DisplayFinalScore.js
+++ src/components/DisplayFinalScore.js
@@ -10,8 +10,8 @@ function DisplayFinalScore(props) {
       <div> <h1 className="title">Monsters & Click</h1> </div>
     <div className="gameover">
       <h1>Game Over</h1>
-      <span>Please insert coin</span>
       <p>Your Score is :{props.score} </p>
+      <span>Try Again!</span>
     </div>
     <div className="DisplayFinalScore-Button"> <ClickButton/> </div>
     </div>
