a8b99ce90b754070051cfa54652e211f7061b08c
Pauline Drouhin <pauline.drouhin@gmail.com>
2019-10-31 01:46:19 +0100

    created MonsterToClick component

---
+++ src/components/MonsterToClick.js
@@ -0,0 +1,15 @@
+import React, {MonsterToClick} from "react"
+
+class MonsterToClick extends Component {
+
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+
+export default MonsterToClick
